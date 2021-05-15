86f990a030d58dee6eecc637e18467ab94141265
demirbileku <udemirbilek2@gmail.com>
2021-05-15 11:02:35 +0300

    commit tree 0b2ba42a2c53e99a7649c55c0cd0cd653e3f1283

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map (array, func) {
+    let r = []
+    for (let i = 0; i < array.length; i++ ) {
+      r.push(func(array[i]));
+    }
+    return r
+}
+
+function reduce (array, func, start = 0) {
+	let memo = start
+    for (let i = 0; i < array.length; i++ ) {
+        if(array[i] === false){
+            memo = false
+            continue
+        } else if (array[i] === true){
+            memo = true
+            continue       
+        }
+      memo = func(array[i],memo);
+    }
+    return memo
+}
